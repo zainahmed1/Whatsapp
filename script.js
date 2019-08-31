672933b9de874368516a8f6114eee84877cb0a67
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-08-31 10:36:30 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,58 +1,4 @@
 
-document.addEventListener("DOMContentLoaded", function(event) {
-
-    function ground() {
-  
-      var tl = new TimelineMax({
-        repeat: -1
-      });
-  
-      tl.to("#ground", 20, {
-          backgroundPosition: "1301px 0px",
-          force3D:true,
-          rotation:0.01,
-          z:0.01,
-          autoRound:false,
-          ease: Linear.easeNone
-        });
-  
-      return tl;
-    }
-  
-    function clouds() {
-  
-      var tl = new TimelineMax({
-        repeat: -1,
-      });
-  
-      tl.to("#clouds", 52, {
-        backgroundPosition: "-2247px bottom",
-        force3D:true,
-        rotation:0.01,
-        z:0.01,
-        //autoRound:false,
-        ease: Linear.easeNone
-      });
-      
-      return tl;
-    }
-  
-    var masterTL = new TimelineMax({
-        repeat: -1.
-    })
-    
-  window.onload = function() {
-    
-    masterTL
-    .add(ground(),0)
-    .add(clouds(),0)
-    .timeScale(0.7)
-    .progress(1).progress(0)
-    .play();
-  
-  };
-    
-  });
 
 
 let handleSucess = function (response) {
@@ -69,12 +15,12 @@ let handleSucess = function (response) {
             <div id="details">
                 <h3>${messages.name}</h3>
                 <p>${messages.number}</p>
-                <p>${messages.firstLine}</p>
+                <p>${messages.firstLine}</p>            
               </div>  
             </div>
             <div id="time">
-            <button>${messages.numbUnread}</button>
             <p>${messages.time}</p>
+            <span id = "span">${messages.numbUnread}</span>
             </div>
         </li>`
         
